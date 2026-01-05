77697f9e0b8d3b14f06486a8f34319f1f75f032d
Daniel Girtler <dgirtler@atlassian.com>
2026-01-05 20:41:29 +1100

    Update

--- a.py
+++ a.py
@@ -1,86 +1,92 @@
 from textual.app import App, ComposeResult
 from textual.geometry import Offset
 from textual.widgets import OptionList
+from textual.screen import Screen
+
 
 def out(text):
-	with open('out.txt', 'a') as f:
-		f.write(text + '\n')
+    with open('out.txt', 'a') as f:
+        f.write(text + '\n')
+
+
+class FruitScreen(Screen):
+    CSS = """
+    .content-container {
+        width: 1fr;
+        height: 1fr;
+        max-height: 100%;
+
+        padding-top: 0;
+        padding-bottom: 3;
+        padding-left: 2;
+
+        background: transparent;
+    }
+
+    .list-container {
+        width: auto;
+        height: auto;
+        max-height: 100%;
+
+        background: transparent;
+    }
+
+    OptionList {
+        width: auto;
+        height: auto;
+        min-width: 15%;
+        max-height: 1fr;
+
+        background: transparent;
+    }
+
+    OptionList > .option-list--option-highlighted {
+        background: blue;
+        color: white;
+        text-style: bold;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        self.app.console.show_cursor(True)
+        yield OptionList()
+
+    def on_mount(self) -> None:
+        self._update_options()
+
+    def _update_options(self) -> None:
+        option_list = self.query_one(OptionList)
+        option_list.clear_options()
+        option_list.add_options(['apple', 'banana', 'pear', 'cucumber', 'pineapple', 'broccoli', 'strawberry'])
+        self._set_cursor()
+
+    def on_option_list_selected(self) -> None:
+        self.app.exit()
+
+    def on_option_list_option_highlighted(self, event: OptionList.OptionHighlighted) -> None:
+        self._set_cursor()
+
+    def _set_cursor(self):
+        option_list = self.query_one(OptionList)
+        index = option_list.highlighted
+
+        out(f'HIGHTLIGHT: {index}')
+
+        if index is None:
+            return
+
+        target_y = option_list.region.y + index + 1 - option_list.scroll_offset.y
+        self.app.cursor_position = Offset(option_list.region.x, target_y)
+
+        out(f'INDEX: {index}')
+        out(f'TARGET: {target_y}')
 
 
 class FruitApp(App):
-	CSS = """
-	.content-container {
-		width: 1fr;
-		height: 1fr;
-		max-height: 100%;
-
-		padding-top: 0;
-		padding-bottom: 3;
-		padding-left: 2;
-
-		background: transparent;
-	}
-
-	.list-container {
-		width: auto;
-		height: auto;
-		max-height: 100%;
-
-		background: transparent;
-	}
-
-	OptionList {
-		width: auto;
-		height: auto;
-		min-width: 15%;
-		max-height: 1fr;
-
-		background: transparent;
-	}
-
-	OptionList > .option-list--option-highlighted {
-		background: blue;
-		color: white;
-		text-style: bold;
-	}
-	"""
-
-
-	def compose(self) -> ComposeResult:
-		self.app.console.show_cursor(True)
-		yield OptionList()
-
-	def on_mount(self) -> None:
-		self._update_options()
-
-	def _update_options(self) -> None:
-		option_list = self.query_one(OptionList)
-		option_list.clear_options()
-		option_list.add_options(['apple', 'banana', 'pear', 'cucumber', 'pineapple', 'broccoli', 'strawberry'])
-		self._set_cursor()
-
-	def on_option_list_selected(self) -> None:
-		self.exit()
-
-	def on_option_list_option_highlighted(self, event: OptionList.OptionHighlighted) -> None:
-		self._set_cursor()
-
-	def _set_cursor(self):
-		option_list = self.query_one(OptionList)
-		index = option_list.highlighted
-
-		out(f'HIGHTLIGHT: {index}')
-
-		if index is None:
-			return
-
-		target_y = option_list.region.y + index+1 - option_list.scroll_offset.y
-		self.app.cursor_position = Offset(option_list.region.x, target_y)
-
-		out(f'INDEX: {index}')
-		out(f'TARGET: {target_y}')
+    def on_mount(self) -> None:
+        self.push_screen(FruitScreen())
 
 
 if __name__ == '__main__':
-	app = FruitApp()
-	app.run()
+    app = FruitApp()
+    app.run()
